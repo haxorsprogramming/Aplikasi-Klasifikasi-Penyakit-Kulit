058351bd7022c4d9a6242d1bf3efc4be9f873b47
Haxorsprogramming <haxorsuinsu@gmail.com>
2020-05-02 11:24:26 +0700

    apa ada dia yg lain

--- www/ladun/login/js/login.js
+++ www/ladun/login/js/login.js
@@ -1,1 +1,29 @@
-document.getElementById('txtUsername').focus();
+$(document).ready(function(){
+  document.getElementById('txtUsername').focus();
+});
+
+var divUtama = new Vue({
+  el : '#divUtama',
+  data : {
+    pengembang : 'Rini Fadillah'
+  }, methods: {
+    masukAtc : function(){
+      loginSistem();
+    }
+  }
+});
+
+function loginSistem()
+{
+  fakeLogin()
+}
+
+function fakeLogin()
+{
+  Swal.fire({
+    title: 'Error server!',
+    text: 'Server tidak bisa diakses..',
+    icon: 'error',
+    confirmButtonText: 'Kembali'
+  })
+}
