553103379a455246cc0ece2209ab7d7e783b4bf5
haxorsprogramming <haxorsuinsu@gmail.com>
2020-09-20 04:57:19 +0700

    update

--- platforms/browser/www/ladun/login/js/login.js
+++ platforms/browser/www/ladun/login/js/login.js
@@ -1,42 +1,60 @@
+// ROUTE 
+var routeToLogin = 'http://api.haxors.or.id/rini/login_proses.php';
 
-$(document).ready(function(){
-
-
-    var capNotif = $('#capNotifLogin');
-    capNotif.hide();
-    $('#txtUsername').focus();
-
-    $('#btnMasuk').click(function(){
-        let username = $('#txtUsername').val();
-        let password = $('#txtPassword').val();
-        
-        if(username === ''){
-          window.alert("Isi username!!");
-          $('#txtUsername').focus();
-        }else if(password === ''){
-          window.alert("Isi password!!");
-           $('#txtPassword').focus();
-        }else{
-          capNotif.show();
-          $('#capNotifLogin').html("Login ...");
-          $.post('prosesLogin.php',{'username':username,'password':password},function(data){
-            var obj = JSON.parse(data);
-            if(obj.status === 1){
-              window.alert("Login berhasil");
-              window.location.assign('mainApp/');
-            }else{
-              window.alert("Username / Password salah!!");
-               $('#txtUsername').val("");
-               $('#txtPassword').val("");
-               $('#txtUsername').focus();
-            }
-          });
-        }
-        
-    });
-
-    function tutupCapNotif(){
-        capNotif.hide();
+// VUE OBJECT 
+var divUtama = new Vue({
+  el :'#divUtama',
+  data : {
+    pengembang : 'Rini Fadillah'
+  },
+  methods : {
+    masukAtc : function()
+    {
+      
+      prosesLogin();
     }
-
+  }
 });
+
+//INISIALISASI
+let statusKoneksi = navigator.onLine;
+document.querySelector('#txtUsername').focus();
+
+if(statusKoneksi === true){
+  $('#capCekServer').html('Terhubung');
+  $('#capCekServer').addClass('badge-info');
+}else{
+  $('#capCekServer').html('Tidak terkoneksi');
+  $('#capCekServer').addClass('badge-warning');
+}
+
+//FUNCTION 
+function prosesLogin()
+{
+  let username = document.querySelector('#txtUsername').value;
+  let password = document.querySelector('#txtPassword').value;
+
+  if(username === '' || password === ''){
+    pesanUmumApp('warning', 'Isi field!!', 'Harap isi username & password');
+  }else{
+    let dataSend = {'username':username, 'password':password}
+    $.post(routeToLogin, dataSend, function(data){
+      let obj = JSON.parse(data);
+      if(obj.status === 'sukses'){
+        window.location.assign('mainApp/main.html');
+      }else{
+        pesanUmumApp('error', 'Gagal', 'Login gagal, periksa kembali username & password..');
+      }
+    });
+  }
+
+}
+
+function pesanUmumApp(icon, title, text)
+{
+  Swal.fire({
+    icon : icon,
+    title : title,
+    text : text
+  });
+}
