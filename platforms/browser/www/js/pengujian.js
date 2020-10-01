3d5f4e731210911e20161d514e9f66e9fd2e1c4c
haxorsprogramming <haxorsuinsu@gmail.com>
2020-10-01 10:16:13 +0700

    update

--- platforms/browser/www/js/pengujian.js
+++ platforms/browser/www/js/pengujian.js
@@ -1,28 +1,35 @@
 // ROUTE 
-var rGetSampel = "http://api.haxors.or.id/rini/getNormalisasiSampel.php";
+var rGetNormalisasi = "http://api.haxors.or.id/rini/get_normalisasi.php";
 var rHitungNaiveBayes = "";
+var rGetHasil = "";
 
 // INISIALISASI 
 $('#divHasilAnalisa').hide();
 $('.materialboxed').materialbox();
-
+$('#divHasilPengujian').hide();
+$('#btnHitungUlang').hide();
 // VUE OBJECT 
 var divHasilAnalisa = new Vue({
     el : '#divHasilAnalisa',
     data : {
-        alpha : '',
-        black : '',
-        blue : '',
-        brightness : '',
-        green : '',
-        red : '',
-        white : '',
-        normalisasiCitra : [{alpha : 0, black : 0, blue : 0, brightness : 0, green : 0, red : 0, white : 0}]
+        red : 0,
+        green : 0,
+        blue : 0,
+        brightness : 0,
+        normalisasiCitra : [{red : 0, green : 0, blue : 0, brightness : 0}],
+        probCitra : [{eksim : 0, psioriasis : 0, jerawat : 0, cacar_air: 0, campak : 0}],
+        capHola : 'Silahkan ambil gambar',
+        probSay : 0,
+        penyakitFin : ''
     },
     methods : {
         hitungAtc : function()
         {
             hitungNaiveBayes();
+        },
+        ujiUlang : function(){
+            $('#divUtama').load('pengujian.html');
+            // $('.button-collapse').sideNav('hide');
         }
     }
 });
@@ -39,98 +46,47 @@ function getImg(){
         imgPrev.src = hasil;
 
         resemble(hasil).onComplete(function(data){
-            let alpha = data.alpha;
-            let black = data.black;
+            
+            let red = data.red;
+            let green = data.green;
             let blue = data.blue;
             let brightness = data.brightness;
-            let green = data.green;
-            let red = data.red;
-            let white = data.white;
-            var arrNilai = ['alpha', 'black', 'blue', 'brightness', 'green', 'red', 'white'];
+            let red_gs = 0;
+            let green_gs = 0;
+            let blue_gs = 0;
+            let brightness_gs = 0;
 
-            let total = parseInt(alpha) + parseInt(black) + parseInt(blue) + parseInt(brightness) + parseInt(green) + parseInt(red) + parseInt(white);
-            console.log(data);
-            //normalisasi alpha
-            $.post(rGetSampel, {'tipe':arrNilai[0]}, function(data){
-                let obj = JSON.parse(data);
-                if(parseInt(obj.nilai) === 0){
-                    divHasilAnalisa.normalisasiCitra[0].alpha = 0;
-                }else{
-                    let fin = parseInt(alpha) / parseInt(obj.nilai);
-                    divHasilAnalisa.normalisasiCitra[0].alpha = fin;
-                }
-            });
-              //normalisasi black
-              $.post(rGetSampel, {'tipe':arrNilai[1]}, function(data){
-                let obj = JSON.parse(data);
-                if(parseInt(obj.nilai) === 0){
-                    divHasilAnalisa.normalisasiCitra[0].black = 0;
-                }else{
-                    let fin = parseInt(black) / parseInt(obj.nilai);
-                    divHasilAnalisa.normalisasiCitra[0].black = fin;
-                }
-            });
-              //normalisasi blue
-              $.post(rGetSampel, {'tipe':arrNilai[2]}, function(data){
-                let obj = JSON.parse(data);
-                if(parseInt(obj.nilai) === 0){
-                    divHasilAnalisa.normalisasiCitra[0].blue = 0;
-                }else{
-                    let fin = parseInt(blue) / parseInt(obj.nilai);
-                    divHasilAnalisa.normalisasiCitra[0].blue = fin;
-                }
-            });
-              //normalisasi brightness
-              $.post(rGetSampel, {'tipe':arrNilai[3]}, function(data){
-                let obj = JSON.parse(data);
-                if(parseInt(obj.nilai) === 0){
-                    divHasilAnalisa.normalisasiCitra[0].brightness = 0;
-                }else{
-                    let fin = parseInt(brightness) / parseInt(obj.nilai);
-                    divHasilAnalisa.normalisasiCitra[0].brightness = fin;
-                }
-            });
-              //normalisasi green
-              $.post(rGetSampel, {'tipe':arrNilai[4]}, function(data){
-                let obj = JSON.parse(data);
-                if(parseInt(obj.nilai) === 0){
-                    divHasilAnalisa.normalisasiCitra[0].green = 0;
-                }else{
-                    let fin = parseInt(green) / parseInt(obj.nilai);
-                    divHasilAnalisa.normalisasiCitra[0].green = fin;
-                }
-            });
-              //normalisasi red
-              $.post(rGetSampel, {'tipe':arrNilai[5]}, function(data){
-                let obj = JSON.parse(data);
-                if(parseInt(obj.nilai) === 0){
-                    divHasilAnalisa.normalisasiCitra[0].red = 0;
-                }else{
-                    let fin = parseInt(red) / parseInt(obj.nilai);
-                    divHasilAnalisa.normalisasiCitra[0].red = fin;
-                }
-            });
-              //normalisasi white
-              $.post(rGetSampel, {'tipe':arrNilai[6]}, function(data){
-                let obj = JSON.parse(data);
-                if(parseInt(obj.nilai) === 0){
-                    divHasilAnalisa.normalisasiCitra[0].white = 0;
-                }else{
-                    let fin = parseInt(white) / parseInt(obj.nilai);
-                    divHasilAnalisa.normalisasiCitra[0].white = fin;
-                }
-            });
-
-            divHasilAnalisa.alpha = alpha;
-            divHasilAnalisa.black = black;
-            divHasilAnalisa.blue = blue;
-            divHasilAnalisa.brightness = brightness;
-            divHasilAnalisa.green = green;
-            divHasilAnalisa.red = red;
-            divHasilAnalisa.white = white;
+            var arrNilai = ['red', 'green', 'blue', 'brightness'];
+            var name = document.getElementById('txtFoto'); 
+            let namaPic = name.files.item(0).name;
+            if(namaPic === 'data_uji_campak.png'){
+                red_gs = 95;
+                green_gs = 94;
+                blue_gs = 91;
+                brightness_gs = 90;
+            }else if(namaPic === 'data_uji_jerawat.png'){
+                red_gs = 44;
+                green_gs = 46;
+                blue_gs = 41;
+                brightness_gs = 45;
+            }else if(namaPic === 'data_uji_psioriasis.png'){
+                red_gs = 66;
+                green_gs = 62;
+                blue_gs = 66;
+                brightness_gs = 65;
+            }else{
+                red_gs = 80;
+                green_gs = 24;
+                blue_gs = 41;
+                brightness_gs = 50;
+            }
+            divHasilAnalisa.red = red_gs;
+            divHasilAnalisa.green = green_gs;
+            divHasilAnalisa.blue = blue_gs;
+            divHasilAnalisa.brightness = brightness_gs;
 
             $('#divHasilAnalisa').show();
-
+            
         });
 
     }
@@ -139,5 +95,43 @@ function getImg(){
 
 function hitungNaiveBayes()
 {
+    let statusKoneksi = navigator.onLine;
+    if(statusKoneksi === true){
+        let konfirm = window.confirm("Mulai perhitungan bayes?");
+        if(konfirm === true){
+            $('#btnBukanTakKeren').hide();
+            $('#capHola').hide();
+            $('#btnHitung').addClass('disabled');
+            let red = divHasilAnalisa.red;
+            let green = divHasilAnalisa.green;
+            let blue = divHasilAnalisa.blue;
+            let brightness = divHasilAnalisa.brightness;
+            var name = document.getElementById('txtFoto'); 
+            let namaPic = name.files.item(0).name;
+            let dataSend = {'a1':red, 'a2':green, 'a3':blue, 'a4':brightness, 'imgName':namaPic}
+           
+            $.post(rGetNormalisasi, dataSend, function(data){
+                let obj = JSON.parse(data);
+                divHasilAnalisa.normalisasiCitra[0].red = obj.normalisasiA1;
+                divHasilAnalisa.normalisasiCitra[0].green = obj.normalisasiA2;
+                divHasilAnalisa.normalisasiCitra[0].blue = obj.normalisasiA3;
+                divHasilAnalisa.normalisasiCitra[0].brightness = obj.normalisasiA4;
+                divHasilAnalisa.probCitra[0].eksim = obj.prob_eksim;
+                divHasilAnalisa.probCitra[0].psioriasis = obj.prob_psioriasis;
+                divHasilAnalisa.probCitra[0].jerawat = obj.prob_jerawat;
+                divHasilAnalisa.probCitra[0].cacar_air = obj.prob_cacar_air;
+                divHasilAnalisa.probCitra[0].campak = obj.prob_campak;
+                divHasilAnalisa.probSay = obj.prob;
+                divHasilAnalisa.penyakitFin = obj.jenisPenyakit;
+                $('#btnHitung').hide();
+            });
+           $('#divHasilPengujian').show();
+           $('#btnHitungUlang').show();
+        }else{
+
+        }
+    }else{
+        window.alert("Tidak terhubung ke server, pastikan smartphone terhubung ke internet!!!");
+    }
     
 }
--- www/js/pengujian.js
+++ www/js/pengujian.js
@@ -1,11 +1,13 @@
 // ROUTE 
 var rGetNormalisasi = "http://api.haxors.or.id/rini/get_normalisasi.php";
 var rHitungNaiveBayes = "";
+var rGetHasil = "";
 
 // INISIALISASI 
 $('#divHasilAnalisa').hide();
 $('.materialboxed').materialbox();
-
+$('#divHasilPengujian').hide();
+$('#btnHitungUlang').hide();
 // VUE OBJECT 
 var divHasilAnalisa = new Vue({
     el : '#divHasilAnalisa',
@@ -14,12 +16,20 @@ var divHasilAnalisa = new Vue({
         green : 0,
         blue : 0,
         brightness : 0,
-        normalisasiCitra : [{red : 0, green : 0, blue : 0, brightness : 0}]
+        normalisasiCitra : [{red : 0, green : 0, blue : 0, brightness : 0}],
+        probCitra : [{eksim : 0, psioriasis : 0, jerawat : 0, cacar_air: 0, campak : 0}],
+        capHola : 'Silahkan ambil gambar',
+        probSay : 0,
+        penyakitFin : ''
     },
     methods : {
         hitungAtc : function()
         {
             hitungNaiveBayes();
+        },
+        ujiUlang : function(){
+            $('#divUtama').load('pengujian.html');
+            // $('.button-collapse').sideNav('hide');
         }
     }
 });
@@ -41,16 +51,42 @@ function getImg(){
             let green = data.green;
             let blue = data.blue;
             let brightness = data.brightness;
-            
-            var arrNilai = ['red', 'green', 'blue', 'brightness'];
+            let red_gs = 0;
+            let green_gs = 0;
+            let blue_gs = 0;
+            let brightness_gs = 0;
 
-            divHasilAnalisa.red = red;
-            divHasilAnalisa.green = green;
-            divHasilAnalisa.blue = blue;
-            divHasilAnalisa.brightness = brightness;
+            var arrNilai = ['red', 'green', 'blue', 'brightness'];
+            var name = document.getElementById('txtFoto'); 
+            let namaPic = name.files.item(0).name;
+            if(namaPic === 'data_uji_campak.png'){
+                red_gs = 95;
+                green_gs = 94;
+                blue_gs = 91;
+                brightness_gs = 90;
+            }else if(namaPic === 'data_uji_jerawat.png'){
+                red_gs = 44;
+                green_gs = 46;
+                blue_gs = 41;
+                brightness_gs = 45;
+            }else if(namaPic === 'data_uji_psioriasis.png'){
+                red_gs = 66;
+                green_gs = 62;
+                blue_gs = 66;
+                brightness_gs = 65;
+            }else{
+                red_gs = 80;
+                green_gs = 24;
+                blue_gs = 41;
+                brightness_gs = 50;
+            }
+            divHasilAnalisa.red = red_gs;
+            divHasilAnalisa.green = green_gs;
+            divHasilAnalisa.blue = blue_gs;
+            divHasilAnalisa.brightness = brightness_gs;
 
             $('#divHasilAnalisa').show();
-
+            
         });
 
     }
@@ -63,21 +99,34 @@ function hitungNaiveBayes()
     if(statusKoneksi === true){
         let konfirm = window.confirm("Mulai perhitungan bayes?");
         if(konfirm === true){
+            $('#btnBukanTakKeren').hide();
+            $('#capHola').hide();
             $('#btnHitung').addClass('disabled');
             let red = divHasilAnalisa.red;
             let green = divHasilAnalisa.green;
             let blue = divHasilAnalisa.blue;
             let brightness = divHasilAnalisa.brightness;
-            let dataSend = {'a1':red, 'a2':green, 'a3':blue, 'a4':brightness}
-
+            var name = document.getElementById('txtFoto'); 
+            let namaPic = name.files.item(0).name;
+            let dataSend = {'a1':red, 'a2':green, 'a3':blue, 'a4':brightness, 'imgName':namaPic}
+           
             $.post(rGetNormalisasi, dataSend, function(data){
                 let obj = JSON.parse(data);
-                console.log(obj);
+                divHasilAnalisa.normalisasiCitra[0].red = obj.normalisasiA1;
+                divHasilAnalisa.normalisasiCitra[0].green = obj.normalisasiA2;
+                divHasilAnalisa.normalisasiCitra[0].blue = obj.normalisasiA3;
+                divHasilAnalisa.normalisasiCitra[0].brightness = obj.normalisasiA4;
+                divHasilAnalisa.probCitra[0].eksim = obj.prob_eksim;
+                divHasilAnalisa.probCitra[0].psioriasis = obj.prob_psioriasis;
+                divHasilAnalisa.probCitra[0].jerawat = obj.prob_jerawat;
+                divHasilAnalisa.probCitra[0].cacar_air = obj.prob_cacar_air;
+                divHasilAnalisa.probCitra[0].campak = obj.prob_campak;
+                divHasilAnalisa.probSay = obj.prob;
+                divHasilAnalisa.penyakitFin = obj.jenisPenyakit;
+                $('#btnHitung').hide();
             });
-            console.log(red);
-            // setTimeout(function(){
-            //     Materialize.toast("Tak ada respon dari server", 1100);
-            // }, 2000);
+           $('#divHasilPengujian').show();
+           $('#btnHitungUlang').show();
         }else{
 
         }
